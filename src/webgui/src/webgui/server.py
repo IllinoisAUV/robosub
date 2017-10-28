19037cdf3c5a47de302a2ff274d0b0a19900eba9
Adrian Brandemuehl <abrandemuehl@gmail.com>
2017-10-27 19:49:28 -0500

    Create basic webserver that is also a ros node
    This will be the baseline for the web gui

---
+++ src/webgui/src/webgui/server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+import rospy
+from std_msgs.msg import String
+import os, rospkg
+
+
+# This is needed in order to find templates, config and static directories due
+# to how catkin handles python file installation. Since it will `exec` this 
+# file from a different location, we must use the ROS API's to locate the real
+# instance_path
+rp = rospkg.RosPack()
+app = Flask(__name__, instance_path=os.path.join(rp.get_path("webgui"), "src", "webgui"))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+def callback(data):
+    print(data.data)
+
+def main():
+    rospy.init_node('webserver')
+
+    rospy.Subscriber('testing', String, callback)
+
+    # We do not need to run rospy.spin() here because all rospy.spin does is
+    # block until the node is supposed to be killed
+    app.run(host="0.0.0.0", debug=True)
