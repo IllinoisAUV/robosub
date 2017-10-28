cc34d1870407e347590f794e22c5c5a3fa5e8953
Adrian Brandemuehl <abrandemuehl@gmail.com>
2017-10-27 21:02:56 -0500

    Fix issue with rospy being loaded twice
    When rospy is allowed to load twice, something goes wrong and it eats up
    cores. This fixes it by only running rospy setup in the child process of
    the flask debug reloader

--- src/webgui/src/webgui/server.py
+++ src/webgui/src/webgui/server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from flask import Flask, render_template
 import rospy
 from std_msgs.msg import String
@@ -24,12 +23,18 @@ def index():
 def callback(data):
     print(data.data)
 
+
 def main():
-    rospy.Subscriber('testing', String, callback)
-
-    rospy.init_node('webserver', disable_signals=True)
-
+    print("main")
+    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+        # When running flask with debug=True, rospy has to be set up in this if
+        # guard because the flask reloader forks and then calls the server, 
+        # which will be killed and restarted on a reload. This causes some 
+        # problems with rospy that I haven't been able to debug
+        # See https://stackoverflow.com/questions/25504149/why-does-running-the-flask-dev-server-run-itself-twice
+        rospy.init_node('webserver', log_level=rospy.DEBUG,  disable_signals=True)
+        rospy.Subscriber('testing', String, callback)
+        rospy.Subscriber("testing2", String, callback)
     # We do not need to run rospy.spin() here because all rospy.spin does is
     # block until the node is supposed to be killed
     app.run(host="0.0.0.0", debug=True)
-
