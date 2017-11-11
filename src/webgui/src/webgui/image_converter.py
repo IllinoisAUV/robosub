e6120e0e650b80502517498abf1b5ce245c826ac
Howard <howardshan11@gmail.com>
2017-11-11 13:13:22 -0600

    Remove unnecessary imports and print statements

--- src/webgui/src/webgui/image_converter.py
+++ src/webgui/src/webgui/image_converter.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python
-from __future__ import print_function
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-import roslib
 import sys
 import rospy
 import cv2
-
-roslib.load_manifest('webgui')
 
 class image_converter:
 
 	def __init__(self):
 		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("image_topic",Image,self.callback, queue_size=10)
+		self.image_sub = rospy.Subscriber('image_topic',Image,self.callback, queue_size=10)
 		
 	def callback(self,data):
-		print('image received')
 		try:
-			cv_image = self.bridge.imgmsg_to_cv2(data, "8UC3")
-		except CvBridgeError as e:
-			print(e)
+			cv_image = self.bridge.imgmsg_to_cv2(data, '8UC3')
+		except CvBridgeError as re:
+			rospy.logerr('Failed to convert img msg to cv2:\n%s', e)
 
 
 def main(args):
@@ -31,7 +25,7 @@ def main(args):
 	try:
 		rospy.spin()
 	except KeyboardInterrupt:
-		print("Shutting down")
+		print('Shutting down')
 	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
