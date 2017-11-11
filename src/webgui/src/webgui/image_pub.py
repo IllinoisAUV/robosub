c3ac9465b3c483131381b11aa56c4009d138c5c3
Howard <howardshan11@gmail.com>
2017-11-11 14:29:17 -0600

    Get left camera video stream to work

---
+++ src/webgui/src/webgui/image_pub.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import rospy
+import time
+
+class image_pub:
+	def __init__(self, args):
+		self.image_pub = rospy.Publisher("image_topic", Image, queue_size=10)
+    		self.bridge = CvBridge()
+		self.cam = cv2.VideoCapture(args[1])
+	
+	def publish_images(self):
+		while self.cam.isOpened() and not rospy.is_shutdown():
+			meta, frame = self.cam.read()
+
+			msg_frame = CvBridge().cv2_to_imgmsg(frame)		
+
+			self.image_pub.publish(msg_frame)
+
+def main(args):
+	if (len(args) == 1):
+		print('No file specified')
+		return
+	rospy.init_node('image_pub')
+	ic = image_pub(args)
+	try:
+		ic.publish_images()
+	except KeyboardInterrupt:
+		print("Shutting down")
+ 	cv2.destroyAllWindows()
+
+	
+
+if __name__ == '__main__':
+    main(sys.argv)
+
--- src/webgui/src/webgui/server.py
+++ src/webgui/src/webgui/server.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 import os
 import sys
 import rospkg
 import threading
+import cv2
 
 
 class Sub:
@@ -26,6 +29,35 @@ app = Flask(__name__, instance_path=os.path.join(rp.get_path("webgui"), "src", "
 def index():
     return render_template('index.html', sub=sub)
 
+# @app.route("/l_camera")
+def cvt_imgmsg_to_cv2(data, bridge):
+	try:
+		cv2_img = bridge.imgmsg_to_cv2(data, '8UC3')
+		ret, jpeg = cv2.imencode('.jpg', cv2_img)
+		if jpeg is not None:
+			return (b'--frame\r\n Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+		else:
+			rospy.logerr("frame is none")
+
+
+	except CvBridgeError as re:
+		rospy.logerr('Failed to convert img msg to cv2:\n%s', e)
+	
+def video_feed():
+	"""Video streaming route. Put this in the src attribute of an img tag."""
+	bridge = CvBridge()
+	while True:
+		rospy.logdebug('Waiting for img')
+		data = rospy.wait_for_message('image_topic', Image)
+		jpeg = cvt_imgmsg_to_cv2(data, bridge)
+		yield(jpeg)
+		
+		
+
+@app.route('/l_camera')
+def l_camera():
+	return Response(video_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+                    
 # Sample callback for ROS to use
 def callback(data):
     print(data.data)
@@ -41,6 +73,8 @@ def main():
         rospy.init_node('webserver', log_level=rospy.DEBUG,  disable_signals=True)
         rospy.Subscriber('testing', String, callback)
         rospy.Subscriber("testing2", String, callback)
+        rospy.Subscriber('l_camera', Image, video_feed)
+        
     # We do not need to run rospy.spin() here because all rospy.spin does is
     # block until the node is supposed to be killed
     app.run(host="0.0.0.0", debug=True)
