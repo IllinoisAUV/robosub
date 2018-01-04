3656840bcda9bb741e9c871f0802e5353589287e
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-01-03 17:45:31 -0800

    Add pylint checker

---
+++ cmake/pylint.cmake
@@ -0,0 +1,12 @@
+#additional target to perform cppcheck run, requires cppcheck
+find_program(PYLINT pylint)
+if(PYLINT)
+  # get all python project files
+  file(GLOB_RECURSE ALL_SOURCE_FILES *.py)
+  MESSAGE( STATUS "${ALL_SOURCE_FILES}")
+  add_custom_target(
+    pylint
+    COMMAND ${PYLINT}
+    ${ALL_SOURCE_FILES}
+  )
+endif()
