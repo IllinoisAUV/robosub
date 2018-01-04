e9edecc2bd68039ed1f651a36cc7fb58f5268fee
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-01-03 17:46:46 -0800

    Add extra python module file
    This is for the case when no other python files exist in the repo to
    satisfy pylint

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# This file is for pylint just in case no other python files exist in the
+# repository. If no python files exist in the repository, the pylint cmake
+# command will always fail due to how it's set up
