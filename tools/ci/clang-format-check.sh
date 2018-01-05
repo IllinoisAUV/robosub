2bf17fbc3c1ab006b2b8e79466ce8fe30b5d931b
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-01-04 18:09:27 -0800

    Use clang format properly

--- tools/ci/clang-format-check.sh
+++ tools/ci/clang-format-check.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -xe
+
 # Takes the list of files to check as an argument, and checks if the files
 # require changes. If they do, this script fails
 clang-format -output-replacements-xml $@ | grep "<replacement " > /dev/null
