e2a52bc3371f87ed3dcfaba034aec5cb0e59ca27
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-01-09 13:47:04 -0800

    Use clang format properly in CI (#32)
    Use clang format properly

--- tools/ci/clang-format-check.sh
+++ tools/ci/clang-format-check.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
+set -xe
+
 # Takes the list of files to check as an argument, and checks if the files
 # require changes. If they do, this script fails
-clang-format -output-replacements-xml $@ | grep "<replacement " > /dev/null
-
-# Previous command does opposite of desired.
-if [ $? -eq 0 ]
-then
-    echo "Clang-format checks did not pass. Please run catkin_make --make-args clangformat"
-    exit 1
-fi
+clang-format -output-replacements-xml $@ | grep "<replacement " > /dev/null && exit 1 || exit 0
