f760abd6405b9624b4424a9e02938ce3df4cb65c
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-01-09 13:51:23 -0800

    Run clang-format on code

--- src/helpers/param.h
+++ src/helpers/param.h
@@ -6,7 +6,7 @@
 
 // Get a parameter. If the parameter is not set, throw an exception
 template <typename T>
-void getParamOrThrow(std::string param, T &target) throw (ros::Exception) {
+void getParamOrThrow(std::string param, T &target) throw(ros::Exception) {
   if (!ros::param::get(param, target)) {
     throw ros::Exception("Parameter " + param + " not specified");
   }
