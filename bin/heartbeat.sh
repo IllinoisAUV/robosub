ec54833657861fdbdbea30bcf38ad2416c8c3544
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-02 10:52:21 -0600

    Make the sub move. Tested on land

--- bin/heartbeat.sh
+++ bin/heartbeat.sh
@@ -2,6 +2,6 @@
 
 while true; do
     echo "Testing connection"
-    ping -W 1 -c1 10.0.0.15 2>&1 > /dev/null && echo "Connected!" || rosservice call /mavros/cmd/arming 0 
+    ping -W 1 -c1 10.0.0.1 2>&1 > /dev/null && echo "Connected!" || rosservice call /mavros/cmd/arming 0 
     sleep 0.5
 done
