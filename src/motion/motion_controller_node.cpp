a49722ac5d8156f0381abeb94684d9355d08bea9
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-11 11:36:38 -0600

    Abstract away motion controller more

--- src/motion/motion_controller_node.cpp
+++ src/motion/motion_controller_node.cpp
@@ -1,6 +1,4 @@
-#include <fcntl.h>
 #include <ros/ros.h>
-#include <signal.h>
 
 #include "helpers/AngularPosition.h"
 #include "helpers/AngularVelocity.h"
@@ -12,42 +10,14 @@
 #include "motion_controller.h"
 #include "rcmavros.h"
 
-// Signal-safe flag for whether shutdown is requested
-sig_atomic_t volatile g_request_shutdown = 0;
-
-// Replacement SIGINT handler
-void sigIntHandler(int sig) { g_request_shutdown = 1; }
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, "motion_controller");
 
-  // This must come after the nodehandle is initialized because any new
-  // NodeHandles will reregister the default signal handlers
-  signal(SIGINT, sigIntHandler);
-
+  // Construction should disable movement
   MotionController *controller = new MavrosRCController();
 
-  ros::NodeHandle nh;
-  ros::Subscriber rpy_sub = nh.subscribe("/setpoint/angular/position", 1,
-                                         &MotionController::SetRPY, controller);
-  ros::Subscriber drpy_sub = nh.subscribe(
-      "/setpoint/angular/velocity", 1, &MotionController::SetdRPY, controller);
-  ros::Subscriber xyz_sub = nh.subscribe("/setpoint/linear/position", 1,
-                                         &MotionController::SetXYZ, controller);
-  ros::Subscriber dxyz_sub = nh.subscribe(
-      "/setpoint/linear/velocity", 1, &MotionController::SetdXYZ, controller);
-  ros::Subscriber kill_sub =
-      nh.subscribe("/arming", 10, &MotionController::arming, controller);
+  controller->Start();
 
-  // Turn off any movement
-  controller->SetdRPY(AngularVelocity(0.0, 0.0, 0.0));
-  controller->SetdXYZ(LinearVelocity(0.0, 0.0, 0.0));
-
-  while (!g_request_shutdown) {
-    ros::spinOnce();
-  }
-
-  controller->SetdRPY(AngularVelocity(0.0, 0.0, 0.0));
-  controller->SetdXYZ(LinearVelocity(0.0, 0.0, 0.0));
-  controller->arming(Bool(false));
+  // Run until ctrl-c is pressed
+  ros::spin();
 }
