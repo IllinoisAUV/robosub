43e582ac83f6573458405935172bc0880dc993f5
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-17 15:00:17 -0600

    Add help function to build.sh and improve readme instructions

--- build.sh
+++ build.sh
@@ -2,6 +2,27 @@
 set -e
 ROOT_DIR=$(dirname $(readlink -f $0))
 
+
+function help() {
+    echo "Usage: $(basename "$0") [options] [target] [args]"
+    echo "Options:"
+    echo "  -h                          Print this help message"
+    echo ""
+    echo "Targets:"
+    echo "  host                        Builds code to be run on host into build/host/"
+    echo "  jetson                      Builds code to be run on jetson into build/jetson/"
+    echo "  clean                       Cleans build folders in build/"
+    echo ""
+    echo "Args:"
+    echo "  --make-args=clangformat     Formats all c++ using clang-format"
+    echo "  --make-args=pylint          Runs pylint on all python code"
+}
+
+if [ "$1" == "-h" ]; then
+    help
+    exit 0
+fi
+
 TARGET=$1
 case $TARGET in
     host);;
@@ -11,7 +32,7 @@ case $TARGET in
         exit 0
         ;;
     *)
-        echo "Please specify a build target among [host, jetson, test]"
+        help
         exit 1
         ;;
 esac
