0e1c905c9f831de345f3007a9fd38f66d21f63d5
ananmay3 <ananmayj8@gmail.com>
2018-02-18 14:24:04 -0600

    Updated barrel roll code frequency of sleep to 10Hz and made velocity zero after the while loop

--- src/experimental/barrelroll.cpp
+++ src/experimental/barrelroll.cpp
@@ -1,42 +1,43 @@
-#include <math.h>
 #include <ros/exception.h>
-#include <ros/ros.h>
 #include <string>
+#include <ros/ros.h>
 #include "helpers/AngularVelocity.h"
+#include <math>
 
-// Initiates a BarrelRoll for one complete circle
 class BarrelRoll {
-  float droll;
-  ros::Publisher angular_vel_pub_;
-  ros::NodeHandle nh_;
-
- public:
-  BarrelRoll(float droll_ = 0) {
-    droll = droll_;
-    std::string angular_vel_topic;
-    if (!ros::param::get("angular_velocity_topic", angular_vel_topic)) {
-      throw ros::Exception("Must specify angular_position_topic parameter");
+    constexpr float droll;
+    ros::Publisher angular_vel_pub_;
+  public:
+    BarrelRoll(float droll_) {
+        droll = droll_;
+        std::string angular_vel_topic;
+        if (!ros::param::get("angular_velocity_topic", angular_vel_topic)) {
+          throw ros::Exception("Must specify angular_position_topic parameter");
+        }
+        angular_vel_pub_ =
+            nh_.advertise<robosub::AngularVelocity>(angular_vel_topic, 1);
     }
-    angular_vel_pub_ =
-        nh_.advertise<robosub::AngularVelocity>(angular_vel_topic, 1);
-  }
-  // Set & publish AngularVelocity with droll_
-  void update() {
-    robosub::AngularVelocity angular = AngularVelocity(droll, 0, 0);
-    angular_vel_pub_.publish(angular);
-  }
+    void update() {
+      robosub::AngularVelocity angular = AngularVelocity(droll, 0, 0);
+
+      angular_vel_pub_.publish(angular);
+    }
+
+    void zeroVelocity() {
+      robosub::AngularVelocity angul = AngularVelocity(0, 0, 0);
+      angular_vel_pub_.publish(angul);
+    }
 };
-int main(int argc, char **argv) {
-  // Value of AngularVelocity
-  float droll = 0.05;
-  ros::init(argc, argv, "barrelroll");
+void main() {
+  droll = 0.05;
   BarrelRoll *broll = new BarrelRoll(droll);
+  ros::Rate loop_rate(10);
   float count = 0;
-  // Publishes velocity till one complete circle
-  while (ros::ok() && count <= (int)(2 * M_PI) / droll) {
-    broll->update();
+  while(ros.ok() && count <= (2*M_PI)*10/droll) {
+    broll.update();
     ros::spinOnce();
     count++;
+    loop_rate.sleep();
   }
-  return 0;
+  broll.zeroVelocity();
 }
