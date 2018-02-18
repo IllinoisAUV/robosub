30d0af60dbd96716388dd3046a053b7c0971d879
ananmay3 <ananmayj8@gmail.com>
2018-02-18 14:13:25 -0600

    Updated barrel roll code to sleep between execution of while loop

--- src/experimental/barrelroll.cpp
+++ src/experimental/barrelroll.cpp
@@ -1,29 +1,30 @@
-#include <ros/exception.h>
-#include <string>
-#include <ros/ros.h>
-#include "helpers/AngularVelocity.h"
 #include <math.h>
+#include <ros/exception.h>
+#include <ros/ros.h>
+#include <string>
+#include "helpers/AngularVelocity.h"
 
 // Initiates a BarrelRoll for one complete circle
 class BarrelRoll {
-    float droll;
-    ros::Publisher angular_vel_pub_;
-    ros::NodeHandle nh_;
-  public:
-    BarrelRoll(float droll_ = 0) {
-        droll = droll_;
-        std::string angular_vel_topic;
-        if (!ros::param::get("angular_velocity_topic", angular_vel_topic)) {
-          throw ros::Exception("Must specify angular_position_topic parameter");
-        }
-        angular_vel_pub_ =
-            nh_.advertise<robosub::AngularVelocity>(angular_vel_topic, 1);
+  float droll;
+  ros::Publisher angular_vel_pub_;
+  ros::NodeHandle nh_;
+
+ public:
+  BarrelRoll(float droll_ = 0) {
+    droll = droll_;
+    std::string angular_vel_topic;
+    if (!ros::param::get("angular_velocity_topic", angular_vel_topic)) {
+      throw ros::Exception("Must specify angular_position_topic parameter");
     }
-    // Set & publish AngularVelocity with droll_
-    void update() {
-      robosub::AngularVelocity angular = AngularVelocity(droll, 0, 0);
-      angular_vel_pub_.publish(angular);
-    }
+    angular_vel_pub_ =
+        nh_.advertise<robosub::AngularVelocity>(angular_vel_topic, 1);
+  }
+  // Set & publish AngularVelocity with droll_
+  void update() {
+    robosub::AngularVelocity angular = AngularVelocity(droll, 0, 0);
+    angular_vel_pub_.publish(angular);
+  }
 };
 int main(int argc, char **argv) {
   // Value of AngularVelocity
@@ -32,7 +33,7 @@ int main(int argc, char **argv) {
   BarrelRoll *broll = new BarrelRoll(droll);
   float count = 0;
   // Publishes velocity till one complete circle
-  while(ros::ok() && count <= (int)(2*M_PI)/droll) {
+  while (ros::ok() && count <= (int)(2 * M_PI) / droll) {
     broll->update();
     ros::spinOnce();
     count++;
