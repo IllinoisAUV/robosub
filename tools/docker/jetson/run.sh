f2750dfceb0c9a3b34f5c73e5043bd01f6c480fa
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-21 21:27:06 -0600

    Make docker containers build files with the correct paths

--- tools/docker/jetson/run.sh
+++ tools/docker/jetson/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run -it -u $(id -u $(whoami)) -v $(pwd):/catkin_ws robosub:jetson catkin_make -DCMAKE_TOOLCHAIN_FILE=/toolchain.cmake -DCMAKE_BUILD_TYPE=Release $@
+docker run -it -u $(id -u $(whoami)) -v $(pwd):/home/ubuntu/catkin_ws -w /home/ubuntu/catkin_ws robosub:jetson catkin_make -DCMAKE_TOOLCHAIN_FILE=/toolchain.cmake -DCMAKE_BUILD_TYPE=Release $@
