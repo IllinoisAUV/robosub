b2b0c5471153248450100264f8a3e1e92f897e6b
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-02-21 21:27:06 -0600

    Make docker containers build files with the correct paths

--- tools/docker/host/run.sh
+++ tools/docker/host/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run -it -u $(id -u $(whoami)) -v $(pwd):/catkin_ws robosub:host catkin_make $@
+docker run -it -u $(id -u $(whoami)) -v $(pwd):$(pwd) -w $(pwd) robosub:host catkin_make $@
