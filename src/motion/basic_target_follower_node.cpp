fc017104af8216c6c636bc1d5bfdf1e01f2e434b
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-03-04 17:33:24 -0600

    Rename nodehandle variable

--- src/motion/basic_target_follower_node.cpp
+++ src/motion/basic_target_follower_node.cpp
@@ -10,10 +10,10 @@ int main(int argc, char **argv) {
   if (!ros::param::get("target_topic", target_topic)) {
     throw ros::Exception("Must specify target_topic parameter");
   }
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
   BasicTargetFollower tgtfollower;
   ros::Subscriber sub =
-      n.subscribe(target_topic, 10, &BasicTargetFollower::update, &tgtfollower);
+      nh.subscribe(target_topic, 10, &BasicTargetFollower::update, &tgtfollower);
   ros::spin();
 
   return 0;
