2ed125ae1a34fc7efb9c039b82541c86c29ce0e0
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-03-13 09:21:48 -0500

    Make parameters for gains

--- src/motion/basic_target_follower_node.cpp
+++ src/motion/basic_target_follower_node.cpp
@@ -10,8 +10,25 @@ int main(int argc, char **argv) {
   if (!ros::param::get("target_topic", target_topic)) {
     throw ros::Exception("Must specify target_topic parameter");
   }
+
+
+  float kSpeed = 0.0;
+  if (!ros::param::get("kSpeed", kSpeed)) {
+    throw ros::Exception("Must specify kSpeed parameter");
+  }
+
+  float kAlt = 0.0;
+  if (!ros::param::get("kAlt", kAlt)) {
+    throw ros::Exception("Must specify kAlt parameter");
+  }
+
+  float kYaw = 0.0;
+  if (!ros::param::get("kYaw", kYaw)) {
+    throw ros::Exception("Must specify kYaw parameter");
+  }
   ros::NodeHandle nh;
-  BasicTargetFollower tgtfollower;
+  BasicTargetFollower tgtfollower(kSpeed, kAlt, kYaw);
+
   ros::Subscriber sub =
       nh.subscribe(target_topic, 10, &BasicTargetFollower::update, &tgtfollower);
   ros::spin();
