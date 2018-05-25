fcf29871d2f495519ef3633904f39d3f73642fcc
Shubhankar Agarwal <sagrwl11@illinois.edu>
2018-05-25 03:45:47 -0700

    kinda works

--- src/motion/basic_target_follower_node.cpp
+++ src/motion/basic_target_follower_node.cpp
@@ -6,26 +6,32 @@
 // (specified by parameter) and published to the motion controller.
 int main(int argc, char **argv) {
   ros::init(argc, argv, "targetfollower");
-  ros::NodeHandle nh("~");
+  std::string target_topic = "/rexrov/target";
+  float kSpeed = 0.40;
+  float kAlt = 0.0010;
+  float kYaw = 0.0005;
+  //
+  // std::string target_topic;
+  // if (!ros::param::get("target_topic", target_topic)) {
+  //   throw ros::Exception("Must specify target_topic parameter");
+  // }
+  //
+  // float kSpeed = 0.0;
+  // if (!ros::param::get("kSpeed", kSpeed)) {
+  //   throw ros::Exception("Must specify kSpeed parameter");
+  // }
+  //
+  // float kAlt = 0.0;
+  // if (!ros::param::get("kAlt", kAlt)) {
+  //   throw ros::Exception("Must specify kAlt parameter");
+  // }
+  //
+  // float kYaw = 0.0;
+  // if (!ros::param::get("kYaw", kYaw)) {
+  //   throw ros::Exception("Must specify kYaw parameter");
+  // }
 
-  // add remap in the launch file
-  std::string target_topic = "/robosub/visual_target";
-
-  float kSpeed = 0.0;
-  if (!nh.getParam("kSpeed", kSpeed)) {
-    throw ros::Exception("Must specify kSpeed parameter");
-  }
-
-  float kAlt = 0.0;
-  if (!nh.getParam("kAlt", kAlt)) {
-    throw ros::Exception("Must specify kAlt parameter");
-  }
-
-  float kYaw = 0.0;
-  if (!nh.getParam("kYaw", kYaw)) {
-    throw ros::Exception("Must specify kYaw parameter");
-  }
-
+  ros::NodeHandle nh;
   BasicTargetFollower tgtfollower(kSpeed, kAlt, kYaw);
 
   ros::Subscriber sub = nh.subscribe(
