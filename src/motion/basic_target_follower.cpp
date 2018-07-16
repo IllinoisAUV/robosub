952ee4013939ac8714c3eb212e3b9bc853726d91
Shubhankar Agarwal <sagrwl11@illinois.edu>
2018-07-16 16:10:48 -0500

    added remaps instead of getting names from launch files

--- src/motion/basic_target_follower.cpp
+++ src/motion/basic_target_follower.cpp
@@ -3,17 +3,14 @@
 #include <ros/exception.h>
 #include <string>
 
-#include "constructors/TwistStamped.h"
+#include <geometry_msgs/Twist.h>
+#include "constructors/Twist.h"
 #include "constructors/Vector3.h"
 
 BasicTargetFollower::BasicTargetFollower(float kSpeed, float kAlt, float kYaw)
     : kSpeed_(kSpeed), kAlt_(kAlt), kYaw_(kYaw) {
-  std::string velocity_topic;
-  if (!ros::param::get("velocity_topic", velocity_topic)) {
-    throw ros::Exception("Must specify angular_position_topic parameter");
-  }
-
-  vel_pub_ = nh_.advertise<geometry_msgs::TwistStamped>(velocity_topic, 1);
+  std::string velocity_topic = "/robosub/target_twist";
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>(velocity_topic, 1);
 }
 
 void BasicTargetFollower::update(const robosub::VisualTarget::ConstPtr& msg) {
@@ -24,8 +21,9 @@ void BasicTargetFollower::update(const robosub::VisualTarget::ConstPtr& msg) {
   // Adjust the yaw to point at the target
   float dyaw = x_err * kYaw_;
   float dalt = y_err * kAlt_;
-  geometry_msgs::TwistStamped vel = TwistStamped(
-      std_msgs::Header(), Twist(Vector3(0, 0, dalt), Vector3(0, 0, dyaw)));
+
+  geometry_msgs::Twist vel =
+      Twist(Vector3(kSpeed_, 0, dalt), Vector3(0, 0, dyaw));
 
   vel_pub_.publish(vel);
 }
