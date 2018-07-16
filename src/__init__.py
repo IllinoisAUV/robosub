06eba4c946936591b9944131c58e7fcd1c41d5de
Shubhankar Agarwal <sagrwl11@illinois.edu>
2018-07-16 16:06:42 -0500

    modified some files for state machine to work in simulation

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,2 @@
-# This file is for pylint just in case no other python files exist in the
-# repository. If no python files exist in the repository, the pylint cmake
-# command will always fail due to how it's set up
+from task_manager import *
+from transform import *
