e60775021451b3c6515f6ac5ef288712bbd22b88
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-07-31 00:24:33 -0700

    Make updates for autonomous runs

--- start-sub.sh
+++ start-sub.sh
@@ -3,14 +3,18 @@
 # Script that starts the submarine. Should be called by hand or on boot by
 # systemd
 
-set -xeu
+set -xu
 
-# Maxmize the jetson's performance
+# Remove old files if they exist
+sudo rm /home/ubuntu/l4t_dfs.conf
+
+# Maximize the jetson's performance
+set -e
 sudo /home/ubuntu/jetson_clocks.sh --store
 trap "sudo /home/ubuntu/jetson_clocks.sh --restore" EXIT
 
 # Add all necessary launch files here
 roslaunch robosub motion.launch &
-roslaunch robosub bottom-camera.launch &
+# roslaunch robosub bottom-camera.launch &
 
 wait
