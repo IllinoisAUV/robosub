1beee7d8ecd69a491a68bcde358989e4030fbee7
Adrian Brandemuehl <abrandemuehl@gmail.com>
2018-08-03 23:51:55 -0700

    Add jerk_node

---
+++ src/sensors/config.py
@@ -0,0 +1,5 @@
+
+class Config(object):
+    imu_topic = '/mavros/imu/data'
+    jerk_topic = '/jerk'
+
---
+++ src/sensors/jerk_node.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+import sys
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32
+
+from config import Config
+
+class accel_graph:
+    def __init__(self):
+        self.first = False
+        self.prev_time = 0
+        self.prev_accel = 0
+        self.count = 0
+        rospy.init_node('accel_graph', anonymous=True)
+        self.sub = rospy.Subscriber(Config.imu_topic, Imu, self.Imucallback)
+        self.pub = rospy.Publisher(Config.jerk_topic, Float32, queue_size = 10)
+
+    def Imucallback(self,data):
+        if not self.first:
+            self.prev_accel = float(data.linear_acceleration.x)
+            self.prev_time = float(data.header.stamp.nsecs)
+            self.first = True
+            self.count += 1
+        else:
+            curr_accel = float(data.linear_acceleration.x)
+            curr_time = float(data.header.stamp.nsecs)
+            self.count += 1
+
+            dt =  abs(curr_time - self.prev_time)/1e7
+            daccel = curr_accel - self.prev_accel
+            jerk = daccel/dt
+
+            self.prev_accel = curr_accel
+            self.prev_time = curr_time
+
+            msg = Float32()
+            msg.data = jerk
+            self.pub.publish(msg)
+
+def main(args):
+    a_g = accel_graph()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main(sys.argv)
